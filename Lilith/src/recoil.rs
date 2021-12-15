7be7384482f7ad2ec55737ea3b77c2fe3e81f420
CommanderAFF <Commander_AFF@protonmail.com>
2021-12-14 21:09:14 -0600

    Recoil Functions

--- Lilith/src/recoil.rs
+++ Lilith/src/recoil.rs
@@ -1,22 +1,28 @@
 pub struct Weapons
 {
-    Assault_Rifle: String,
-    Assault_Rifle_Time: String,
-    LR_300: String,
-    Semi_Auto_Rifle: String,
-    Semi_Auto_Rifle_Time: String,
-    M39_Rifle: String,
-    M39_Rifle_Time: String,
-    M249_MG: String,
-    M249_MG_Time: String,
-    MP5A4: String,
-    MP5A4_Time: String,
-    Thompson_SMG: String,
-    Thompson_SMG_Time: String,
-    Custom_SMG: String,
-    Custom_SMG_Time: String,
-    Python_Revolver: String,
-    Python_Revolver_Time: String,
-    Semi_Auto_Pistol: String,
-    Semi_Auto_Pistol_Time: String,
+ assault_rifle: String,
+ assault_rifle_time: String,
+ lr_300: String,
+ lr_300_time: String,
+ semi_auto_rifle: String,
+ semi_auto_rifle_time: String,
+ m39_rifle: String,
+ m39_rifle_time: String,
+ m249: String
+ m249_time: String,
+ mp5: String,
+ mp5_time: String,
+ thompson: String,
+ thompson_string: String,
+ custom: String,
+ custom_time: String,
+ python: String,
+ python_time: String
 }
+
+
+match Menu.weapon{
+    "Assault Rifle" => return(AssaultRifle, AssaultRifleTime),
+    "M249" => return(M249, M249Time),
+    _ => return(defaultnothing, 0),
+  };
